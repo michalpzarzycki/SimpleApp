246810ea698bff3699fbec5b38fb8730dffb468c
Michal <michal.p.zarzycki@wp.pl>
2021-04-03 15:37:10 +0200

    Add routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const routes = require('./routes')
 const PORT = process.env.PORT || 3000;
+
 //express app
 const app = express();
+app.use('/', routes)
 
 
 //http server
