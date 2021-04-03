cc4981fe53b1b2eb8bb021adc5f58016515d9eac
Michal <michal.p.zarzycki@wp.pl>
2021-04-03 15:25:38 +0200

    Set up server

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3000;
+//express app
 const app = express();
 
-module.exports = app;
+
+//http server
+const server = app.listen(PORT, () => {
+    console.log(`server running on port ${server.address().port}`)
+})
