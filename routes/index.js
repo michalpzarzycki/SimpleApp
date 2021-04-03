e7d6a32271a5dbd0b4f4fca2d1ff55746bba3318
Michal <michal.p.zarzycki@wp.pl>
2021-04-03 16:15:20 +0200

    Added layout and post method

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('form')
+    res.render('form', {title: "Register"})
+})
+router.post('/', (req, res) => {
+    res.render('form', {title: "Register"})
 })
 
 module.exports = router
